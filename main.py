4522bfdad4d7134f4463c50947705a4c70b05e5b
EnzoAguirre04 <ea.aguirre@alumno.um.edu.ar>
2025-06-17 17:41:23 -0300

    Issue para la Implementación del CLI

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ Ciclo Lectivo: 2025.
 Carrera: Ingeniería en Informática.
 Ruta: "computaci-n-2025-05-27-gesti-n-cl-nica-EnzoAguirre04/main.py".
 """
+# Inicio del código.
+
+from src.cli import CLI
+
+if __name__ == "__main__":
+    CLI().iniciar()
+
+# Fin del código.
